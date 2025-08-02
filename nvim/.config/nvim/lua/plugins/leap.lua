4a270450dfdf6d82c96257be4dbb9b45d57bb332
Ashik R. <ashik.se@proton.me>
2025-08-02 12:56:39 +0600

    feat(nvim): Integrate flash.nvim for enhanced motion
    Integrate `flash.nvim` as a replacement for `leap.nvim` to provide more powerful and flexible motion and search capabilities. This change also updates keybindings for `substitute.nvim` to avoid conflicts and leverage `flash.nvim`'s features.

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -1,3 +1,6 @@
+if true then
+  return {}
+end
 return {
   "ggandor/leap.nvim",
   event = "VeryLazy",
--- nvim/.config/nvim/lua/plugins/substitute.lua
+++ nvim/.config/nvim/lua/plugins/substitute.lua
@@ -9,15 +9,15 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-    keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-    keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-    keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+    -- keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
+    -- keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
+    -- keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+    -- keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
 
-    -- Update due to leap.nvim
-    -- keymap.set("n", "gs", substitute.operator, { desc = "Substitute with motion" })
-    -- keymap.set("n", "gss", substitute.line, { desc = "Substitute line" })
-    -- keymap.set("n", "gS", substitute.eol, { desc = "Substitute to end of line" })
-    -- keymap.set("x", "gs", substitute.visual, { desc = "Substitute in visual mode" })
+    -- Update due to flash.nvim
+    keymap.set("n", "gs", substitute.operator, { desc = "Substitute with motion" })
+    keymap.set("n", "gss", substitute.line, { desc = "Substitute line" })
+    keymap.set("n", "gS", substitute.eol, { desc = "Substitute to end of line" })
+    keymap.set("x", "gs", substitute.visual, { desc = "Substitute in visual mode" })
   end,
 }
