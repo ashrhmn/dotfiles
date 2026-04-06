5b7e107167c17b437c974993271f9a9d69e31434
Ashik R. <ashik.se@proton.me>
2026-04-06 15:51:40 +0600

    disable neotree git status on macos

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -8,6 +8,8 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
+    local is_macos = (vim.uv or vim.loop).os_uname().sysname == "Darwin"
+
     -- If you want icons for diagnostic errors, you'll need to define them somewhere:
     vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
     vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
@@ -16,7 +18,7 @@ return {
     require("neo-tree").setup({
       close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
       popup_border_style = "rounded",
-      enable_git_status = true,
+      enable_git_status = not is_macos, -- git status check causing 30-40x slower neotree invoke times on macOS, possibly related to APFS, TODO:
       enable_diagnostics = true,
       open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
       sort_case_insensitive = false, -- used when sorting files and directories in the tree
