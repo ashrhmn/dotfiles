bcb7b948ee72cee3963ccf965673fc44239942ac
Ashik R. <ashik.se@proton.me>
2026-04-06 15:51:32 +0600

    fix comment invoke on neotree window

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -7,13 +7,22 @@ return {
 	config = function()
 		-- import comment plugin safely
 		local comment = require("Comment")
+		local ts_context_commentstring = require("ts_context_commentstring")
 
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+		-- Only calculate commentstring when commenting.
+		-- The plugin's CursorHold autocmd is both unnecessary with Comment.nvim
+		-- integration and is the source of occasional nil parser errors on
+		-- nonstandard buffers like neo-tree.
+		ts_context_commentstring.setup({
+			enable_autocmd = false,
+		})
+
+		local integration = require("ts_context_commentstring.integrations.comment_nvim")
 
 		-- enable comment
 		comment.setup({
 			-- for commenting tsx, jsx, svelte, html files
-			pre_hook = ts_context_commentstring.create_pre_hook(),
+			pre_hook = integration.create_pre_hook(),
 		})
 	end,
 }
