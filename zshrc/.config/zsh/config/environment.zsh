2fcbfb89f7fb083053a46c40dc670a89e7466e8a
Ashik R. <ashik.se@proton.me>
2026-04-09 14:03:18 +0600

    change zsh dir

---
+++ zshrc/.config/zsh/config/environment.zsh
@@ -0,0 +1,16 @@
+# Environment variables
+
+export TERM='xterm'
+
+# Editor
+export EDITOR='nvim'
+
+# Locale
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+
+# PostgreSQL pager
+export PSQL_PAGER='pspg -X -s 16'
+
+# Dotnet
+export DOTNET_WATCH_RESTART_ON_RUDE_EDIT=1
---
+++ zshrc/.config/zsh/config/paths.zsh
@@ -0,0 +1,5 @@
+# PATH modifications
+
+# User bin
+[[ ":$PATH:" != *":$HOME/.bin:"* ]] && PATH="$HOME/.bin:$PATH"
+
