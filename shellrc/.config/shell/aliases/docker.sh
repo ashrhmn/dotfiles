d1d270144c00517b15b73f80f19718c4bdd6b561
Ashik R. <ashik.se@proton.me>
2026-04-12 06:54:30 +0600

    add more docker alias

--- shellrc/.config/shell/aliases/docker.sh
+++ shellrc/.config/shell/aliases/docker.sh
@@ -8,3 +8,27 @@ alias dpsv="docker ps --format \"$_DPS_FULL_FMT\""
 alias dpsvv="docker ps --format \"$_DPS_VERBOSE_FMT\""
 alias dpsva="docker ps -a --format \"$_DPS_FULL_FMT\""
 alias dpsvva="docker ps -a --format \"$_DPS_VERBOSE_FMT\""
+
+alias dex="docker exec -it"
+alias dlogs="docker logs -f"
+alias dstop="docker stop"
+alias drm="docker rm"
+alias drestart="docker restart"
+
+alias di="docker images"
+alias drmi="docker rmi"
+alias dpull="docker pull"
+alias dbuild="docker build"
+
+alias dcu="docker compose up -d"
+alias dcuf="docker compose up -d --force-recreate --remove-orphans"
+alias dcd="docker compose down"
+alias dcl="docker compose logs -f"
+alias dcb="docker compose build"
+alias dcr="docker compose restart"
+
+alias dprune="docker system prune -f"
+alias dprune-all="docker system prune -af --volumes"
+
+alias dstats="docker stats --no-trunc"
+alias dins="docker inspect"
