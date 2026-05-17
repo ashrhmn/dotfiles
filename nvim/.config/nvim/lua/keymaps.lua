2c8b07d7e9b5851afb962f6ced3989ada46b00c1
Ashik R. <ashik.se@proton.me>
2026-05-17 10:40:22 +0600

    add rotate split

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -59,6 +59,14 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>st", function()
+  local layout = vim.fn.winlayout()
+  if layout[1] == "row" then
+    vim.cmd("wincmd K")
+  else
+    vim.cmd("wincmd H")
+  end
+end, { desc = "Toggle split layout vertical/horizontal" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
