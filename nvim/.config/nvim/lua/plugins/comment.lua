67989354b1853e442997727d0c8dd46e37988d28
Ashik R. <ashik.se@proton.me>
2026-05-18 15:07:54 +0600

    replace comment system

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,28 +1,19 @@
 return {
-	"numToStr/Comment.nvim",
+	"echasnovski/mini.comment",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
 	config = function()
-		-- import comment plugin safely
-		local comment = require("Comment")
-		local ts_context_commentstring = require("ts_context_commentstring")
+		require("ts_context_commentstring").setup({ enable_autocmd = false })
 
-		-- Only calculate commentstring when commenting.
-		-- The plugin's CursorHold autocmd is both unnecessary with Comment.nvim
-		-- integration and is the source of occasional nil parser errors on
-		-- nonstandard buffers like neo-tree.
-		ts_context_commentstring.setup({
-			enable_autocmd = false,
-		})
-
-		local integration = require("ts_context_commentstring.integrations.comment_nvim")
-
-		-- enable comment
-		comment.setup({
-			-- for commenting tsx, jsx, svelte, html files
-			pre_hook = integration.create_pre_hook(),
+		require("mini.comment").setup({
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
 		})
 	end,
 }
